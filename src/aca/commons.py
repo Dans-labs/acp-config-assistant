167d3809c9fe961a1871be56a9459f8d7ead1804
ekoi <eko.indarto@dans.knaw.nl>
2025-03-04 10:11:48 +0100

    Initial version: Refactoring version of https://github.com/ekoi/repository-assistant-service

---
+++ src/aca/commons.py
@@ -0,0 +1,47 @@
+import json
+import logging
+import os
+
+import tomli
+from akmi_utils.models import ras
+from akmi_utils import commons as a_commons
+from dynaconf import Dynaconf
+from pydantic import ValidationError
+
+
+settings = Dynaconf(settings_files=["conf/settings.toml", "conf/*.yaml", "conf/.secrets.toml"],
+                    environments=True)
+data = {}
+
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ["BASE_DIR"] = os.getenv("BASE_DIR", base_dir)
+
+project_details = a_commons.get_project_details(os.getenv("BASE_DIR"), ['name', 'version', 'description', 'title'])
+
+def installed_repos_configs():
+    logging.debug("startup")
+
+    for repo_conf_filename in os.listdir(settings.repositories_conf_dir):
+        if repo_conf_filename.endswith('.json'):
+            with open(os.path.join(settings.repositories_conf_dir, repo_conf_filename)) as f:
+                try:
+                    print(f'repo_conf_filename: {repo_conf_filename}')
+                    saved_repo_assistant = json.loads(f.read())
+                    repo_assistant = ras.RepoAssistantDataModel.model_validate(saved_repo_assistant)
+                    if repo_assistant.assistant_config_name in data:
+                        logging.warning(
+                            f"Configuration {repo_assistant.assistant_config_name} already exists and will be overwritten.")
+                        print(f'>>>> Configuration {repo_assistant.assistant_config_name} already exists and will be overwritten.')
+                    data.update({repo_assistant.assistant_config_name: repo_assistant})
+                    logging.info(f"Loaded valid: {repo_assistant.assistant_config_name} in {repo_conf_filename}")
+                    print(f"Loaded valid: {repo_assistant.assistant_config_name} in {repo_conf_filename}")
+                except json.JSONDecodeError as e:
+                    logging.error(f"Error loading {repo_conf_filename}: {e}")
+                    print(f">>> Error: {repo_conf_filename}, caused by {e}")
+                    continue
+                except ValidationError as e:
+                    logging.error(f"Error validating: {e}")
+                    print(f">>> Error: {repo_conf_filename}, caused by {e}")
+                    continue
+                
+
---
+++ src/main.py
@@ -0,0 +1,116 @@
+import json
+import logging
+import os
+import sys
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+import emoji
+import jmespath
+import uvicorn
+from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from keycloak import KeycloakOpenID, KeycloakAuthenticationError
+from starlette import status
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.middleware.cors import CORSMiddleware
+
+from src.aca import protected, public
+from src.aca.commons import data, settings, installed_repos_configs, project_details
+from akmi_utils import commons as a_commons
+
+api_keys = [settings.DANS_REPO_ASSISTANT_SERVICE_API_KEY]
+security = HTTPBearer()
+
+APP_NAME = os.environ.get("APP_NAME", "Repository Assistant Service")
+EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 2810)
+OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://localhost:4317")
+
+def auth_header(request: Request, auth_cred: Annotated[HTTPAuthorizationCredentials, Depends(security)]):
+    if not auth_cred or auth_cred.credentials not in api_keys:
+        keycloak_env = settings.get(f"keycloak_{request.headers.get('auth-env-name', 'local')}")
+        if not keycloak_env:
+            logging.error(f"keycloak_env: {keycloak_env}")
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden")
+
+        keycloak_openid = KeycloakOpenID(server_url=keycloak_env.URL, client_id=keycloak_env.CLIENT_ID,
+                                         realm_name=keycloak_env.REALMS)
+        try:
+            keycloak_openid.userinfo(auth_cred.credentials)
+            logging.info(f"Keycloak: {keycloak_openid}")
+        except KeycloakAuthenticationError:
+            logging.error(f"KeycloakAuthenticationError: {keycloak_openid}")
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Forbidden")
+        except Exception as e:
+            logging.info(f"Error: {e}")
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    logging.info('start up')
+    installed_repos_configs()
+    logging.info(f'Available repositories configurations: {sorted(list(data.keys()))}')
+    logging.info(emoji.emojize(':thumbs_up:'))
+    with open(settings.repo_file_types) as file:
+        file_types = json.load(file)
+    values = jmespath.search('[*].value', file_types)
+    data.update({"file-types": values})
+    yield
+
+app = FastAPI(title= project_details['title'], description = project_details['description'],
+              version= project_details['version'], lifespan=lifespan)
+
+LOG_FILE = settings.LOG_FILE
+log_config = uvicorn.config.LOGGING_CONFIG
+
+if settings.otlp_enable is False:
+    logging.basicConfig(filename=settings.LOG_FILE, level=settings.LOG_LEVEL,
+                        format=settings.LOG_FORMAT)
+else:
+    a_commons.set_otlp(app, APP_NAME, OTLP_GRPC_ENDPOINT, LOG_FILE, log_config)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        logging.error(f"404 Not Found: {request.url}")
+        return JSONResponse(
+            status_code=404,
+            content={"message": "Endpoint not found"}
+        )
+    logging.error(f"HTTP Exception: {exc.status_code} - {exc.detail}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail}
+    )
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(
+    public.router,
+    tags=["Public"],
+    prefix=""
+)
+
+app.include_router(
+    protected.router,
+    tags=["Protected"],
+    prefix="",
+    dependencies=[Depends(auth_header)]
+)
+
+
+if __name__ == "__main__":
+    logging.info("RAS: Starting the app __main__")
+    uvicorn.run(app, host="0.0.0.0", port=EXPOSE_PORT, log_config=log_config)
